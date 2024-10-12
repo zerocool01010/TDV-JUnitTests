7196d73083809d7ed6cd740a7efff4b8c2c84d07
Manuel Alvarez <mrmanuelalvarez@gmail.com>
2024-10-12 02:02:21 -0300

    tests mejorados

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -1,20 +1,32 @@
 import org.example.Calculadora;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculadoraTest {
-
-    @Test
-    void testSumarTieneQueDarBien() {
-        Calculadora calculadora = new Calculadora();
-        int resultado = calculadora.sumar(2, 3);
-        assertEquals(5, resultado, "La suma de 2 y 3 debería ser 5");
+    Calculadora calc = new Calculadora();
+    public int randomIntCalc(int origin, int bound){
+        Random r = new Random();
+        int entero = r.nextInt(origin, bound);
+        return entero;
     }
 
     @Test
-    void testSumarTieneQueDarMal(){
-        Calculadora calc = new Calculadora();
-        int res = calc.sumar(3, 4);
-        assertEquals(9, res, "La suma deberia ser 9");
+    void testSumaPositiva() {
+        int resultado = calc.sumar(randomIntCalc(0, 100), randomIntCalc(0, 100));
+        //assertEquals(5, resultado, "La suma de 2 y 3 debería ser 5");
+        assertTrue(resultado > 0, "El resultado debe ser positivo");
+        assertNotEquals(0, resultado, "El resultado debe ser diferente de 0");
+        assertFalse(resultado < 0, "El resultado no debe ser negativo");
+    }
+
+    @Test
+    void testSumaNegativa(){
+        int resultado = calc.sumar(randomIntCalc(-100, 0), randomIntCalc(-100, 0));
+        assertFalse(resultado > 0, "El resultado debe ser negativo");
+        assertNotEquals(0, resultado, "El resultado debe ser diferente de 0");
+        assertTrue(resultado < 0, "El resultado no debe ser positivo");
     }
 }
